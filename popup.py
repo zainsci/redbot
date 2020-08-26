0d75d26a1492ddba0c1da2926d20f92e083626de
ZainShabbir <zainansari08@gmail.com>
2020-08-26 11:30:20 +0500

    change popup window shape

--- popup.py
+++ popup.py
@@ -16,22 +16,39 @@ def popup_window(title, permalink):
     window = Tk()
     window.geometry("400x200")
     window.title("Reddit Notification")
-    window.rowconfigure(0, minsize=400, weight=1)
-    window.columnconfigure(0, minsize=150, weight=1)
+    window.configure(background="#ffffff")
 
-    fr_title = tk.Frame(master=window, height=150, bg="#ffffff")
+    frame = tk.Frame(window, bg="#ffffff")
+    frame.pack(side=TOP)
 
-    lbl_title = tk.Label(fr_title, text=title,
-                         bg="#ffffff", fg="#000000")
-    btn_open = tk.Button(master=window, text="Open", height=1, width=5, bg="#7952B3", fg="#fff",
-                         command=lambda: open_reddit(permalink, window))
+    lbl_title = tk.Label(
+        frame,
+        text=title,
+        wraplength=300,
+        font=(None, 16),
+        justify=CENTER,
+        bg="#ffffff",
+        fg="#000000",
+    )
+    btn_open = tk.Button(
+        frame,
+        text="Open",
+        height=1,
+        width=10,
+        font=(None, 16),
+        bg="#7952B3",
+        fg="#fff",
+        command=lambda: open_reddit(permalink, window),
+    )
 
-    fr_title.grid(row=0, column=0, stick="n")
-    lbl_title.grid(row=0, column=0, padx=10, pady=10)
-    btn_open.grid(row=0, column=0, pady=150, sticky="n")
+    lbl_title.grid(row=0, column=0, pady=10, sticky=N)
+    btn_open.grid(row=1, column=0, pady=10, sticky=N)
 
     window.mainloop()
 
 
 if __name__ == "__main__":
-    popup_window(title="How To Work With Tkinter", permalink="/r/Askreddit")
+    popup_window(
+        title="How To Work With TkinterHow To Work With TkinterHow To Work With TkinterHow To Work With Tkinter",
+        permalink="/r/Askreddit",
+    )
